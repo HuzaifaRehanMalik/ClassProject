1fc6b9444130573fb758f2f23730a8f8555f10e7
Huzaifa <157941777+HuzaifaRehanMalik@users.noreply.github.com>
2024-03-06 14:36:58 +0500

    Add files via upload

---
+++ class4_project.ts
@@ -0,0 +1,19 @@
+let percentage=90
+if (percentage>=80){
+    console.log("you have Grade A+ ");
+    }
+else if(percentage>=70){
+    console.log("you have Grade A");
+}
+else if(percentage>=60){
+    console.log("you have Grade B");
+}
+else if(percentage>=50){
+    console.log("you have Grade C");
+}
+else if(percentage>=40){
+    console.log("you have Grade D");
+}
+else{
+    console.log("fail");
+}
